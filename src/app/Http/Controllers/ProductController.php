8eb0dd0434ce2fc4bcef6fd678efc0192f0809a7
SvartNewSki <MakhmudovAmir@gmail.com>
2026-07-29 13:29:38 +0300

    add not availible func

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -8,7 +8,12 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index(){
-        $productList = Product::where('amount', '>=', 1)->get();
+        // $productList = Product::where('amount', '>=', 1)->get();
+        $productList = Product::all();
         return view('main', ['products' => $productList]);
+    }
+    public function item($id){
+        $item = Product::findOrFail($id);
+        return view('item', ['item' => $item]);
     }
 }
--- src/resources/views/main.blade.php
+++ src/resources/views/main.blade.php
@@ -3,10 +3,15 @@
 @section('content')
     @foreach($products as $product)
         <div class="product-item">
-            <h3>{{ $product->name }}</h3>
+            <h3><a href="/item/{{$product->id}}">{{ $product->name }}</a></h3>
             <p>Наличие: {{ $product->amount }} шт.</p>
             <p>{{ $product->description }}</p>
-            <a href="/addToCart/{{$product->id}}">добавить</a>
+            @if ($product->amount >= 1)
+                <a href="/addToCart/{{$product->id}}">добавить</a>
+            @else 
+                Товар закончился
+            @endif
+            <a href="/clear"><span>Удалить</span>_clear_<span>Удалить</span></a>
             
         </div>
     @endforeach
